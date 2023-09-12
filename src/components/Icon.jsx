97302ad13efbe70956185d276987e786b92dfde1
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-13 00:19:29 +0530

    working on first view

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 function Icon({lable, url, element}) {
   return (
-    <li className="text-blue-500 hover:text-blue-700">
+    <li className="text-blue-500 hover:text-blue-700 pb-2 relative before:absolute before:h-0.5 before:w-0 before:bottom-0 before:bg-blue-500 hover:before:w-full focus:before:w-full hover:before:transition-[width] hover:before:duration-300 before:ease-in before:duration-300 before:rounded-sm">
       <a
         href={url}
         aria-label={lable}
