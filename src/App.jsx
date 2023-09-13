15fdf2664a1c49c8a6b2fba34dae14c305c251a0
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-13 17:27:10 +0530

    working on timeline section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Home from "./components/home"
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Timeline from "./components/Timeline";
 
 function App() {
+
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
 
   return (
     <>
       <Home />
+      <Timeline />
     </>
   )
 }
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,144 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import {
+  MdOutlineWork,
+  MdSchool,
+  MdOutlineSchool,
+  MdStars,
+} from "react-icons/md";
+
+function Timeline() {
+  return (
+    <section className="flex items-center flex-col">
+      <h1 data-aos='fade-left' className="text-3xl font-bold leading-9 tracking-wide my-7">My Timeline 💫</h1>
+      <VerticalTimeline lineColor={"black"}>
+        <VerticalTimelineElement
+          date={"January 2023 - Present"}
+          contentStyle={{
+            boxShadow: 'var(--shadow)',
+            backgroundColor: `var(--card-bg-left)`,
+            textAlign: "center",
+            color: `var(--hd-clr)`,
+          }}
+          contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+          iconStyle={{
+            backgroundColor: `var(--clr-bg)`,
+            color: `var(--clr-primary)`,
+          }}
+          icon={<MdOutlineWork />}
+        >
+          <h3 className="vertical-timeline-element-title" data-aos="fade-right">
+            Instructional Associate
+          </h3>
+          <h4
+            className="vertical-timeline-element-subtitle"
+            data-aos="fade-right"
+          >
+            Masai School
+          </h4>
+          <p data-aos="fade-right">
+            Mentoring and clearing student's conceptual doubts via
+            standups,reviewing student's projects, reporting to the instructor.
+            Helping students to start their career in Tech.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          date={"April 2021 - October 2021"}
+          contentStyle={{
+            boxShadow: `var(--shadow)`,
+            backgroundColor: `var(--clr-bg)`,
+            textAlign: "center",
+            color: `var(--clr-fg-alt)`,
+          }}
+          contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+          iconStyle={{
+            backgroundColor: `var(--clr-bg)`,
+            color: `var(--clr-primary)`,
+          }}
+          icon={<MdSchool />}
+        >
+          <h3 className="vertical-timeline-element-title" data-aos="fade-right">
+            Student
+          </h3>
+          <h4
+            className="vertical-timeline-element-subtitle"
+            data-aos="fade-right"
+          >
+            Masai School (Remote)
+          </h4>
+          <p data-aos="fade-right">
+            Joined the Military style Bootcamp to learn Software Development and
+            Data Structures. Also learned many soft skills in the process.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date={"2014 - 2017"}
+          contentStyle={{
+            boxShadow: `var(--shadow)`,
+            backgroundColor: `var(--clr-bg)`,
+            textAlign: "center",
+            color: `var(--clr-fg-alt)`,
+          }}
+          contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+          iconStyle={{
+            backgroundColor: `var(--clr-bg)`,
+            color: `var(--clr-primary)`,
+          }}
+          icon={<MdSchool />}
+        >
+          <h3 className="vertical-timeline-element-title" data-aos="fade-right">
+            Bachelor of Science, Zoology Honors
+          </h3>
+          <h4
+            className="vertical-timeline-element-subtitle"
+            data-aos="fade-right"
+          ></h4>
+          <p data-aos="fade-right">
+            North Bengal St. Xavier's College, Rajganj
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date={"March 2014"}
+          contentStyle={{
+            boxShadow: `var(--shadow)`,
+            backgroundColor: `var(--clr-bg)`,
+            textAlign: "center",
+            color: `var(--clr-fg-alt)`,
+          }}
+          contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+          iconStyle={{
+            backgroundColor: `var(--clr-bg)`,
+            color: `var(--clr-primary)`,
+          }}
+          icon={<MdOutlineSchool />}
+        >
+          <h3 className="vertical-timeline-element-title" data-aos="fade-right">
+            Completed Higher Secondary - Science
+          </h3>
+          <h4
+            className="vertical-timeline-element-subtitle"
+            data-aos="fade-right"
+          ></h4>
+          <p data-aos="fade-right">
+            Jalpaiguri Zilla School, Jalpaiguri, West Bengal
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          icon={<MdStars />}
+          iconStyle={{
+            backgroundColor: `var(--clr-bg)`,
+            color: `var(--clr-primary)`,
+          }}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </section>
+  );
+}
+
+export default Timeline;
