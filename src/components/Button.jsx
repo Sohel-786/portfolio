4ab41ba6375fbfa6e79a2a4f9ef1dcc561f697f2
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-15 16:17:32 +0530

    working on Project Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,18 @@
-function Button({live, git}){
-    return (
-        <div>
-            <a href={live}><button>See This Live</button></a>
-            <a href={git}><button>View Code</button></a>
-        </div>
-    )
+function Button({ live, git }) {
+  return (
+    <div className="flex gap-6 pl-7">
+      <a href={live}>
+        <button className="border-2 border-blue-400 py-3 px-4 font-bold text-sm" style={{
+            backgroundColor : 'var(--card-bg-right)',
+        }}>
+          See This Live
+        </button>
+      </a>
+      <a href={git}>
+        <button>View Code</button>
+      </a>
+    </div>
+  );
 }
 
 export default Button;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,16 @@
 import { nanoid } from "nanoid";
 import Button from "./Button";
+import { IoMdArrowDropright } from "react-icons/io";
 
 function ProjectCard({ url, alt, title, points, tools, liveUrl, gitUrl }) {
   return (
-    <div className="flex gap-6 h-[500px] py-7 px-5 justify-center items-center rounded-2xl border-2 border-red-400">
-
+    <div
+      className="flex gap-6 h-[500px] py-7 px-5 justify-center items-center rounded-2xl border-2 border-sky-200"
+      style={{
+        boxShadow: "var(--shadow)",
+      }}
+    >
       <div className="flex flex-col justify-center items-center gap-6 w-9/12 h-full">
-
         <div className="rounded-2xl overflow-hidden shadow-md shadow-sky-300 h-5/6">
           <img className="w-full h-full" src={url} alt={alt} />
         </div>
@@ -25,14 +29,30 @@ function ProjectCard({ url, alt, title, points, tools, liveUrl, gitUrl }) {
         </div>
       </div>
 
-      <div className="w-6/12 h-full flex flex-col py-7" style={{
-        color : 'var(--clr-mode-text)'
-      }}>
-
-        <h1 className="text-4xl font-bold font-sans text-purple-500">{title}</h1>
+      <div
+        className="w-6/12 h-full flex flex-col py-5"
+        style={{
+          color: "var(--clr-mode-text)",
+        }}
+      >
+        <h1
+          className="text-4xl font-bold font-sans"
+          style={{
+            color: "var(--clr-mode)",
+          }}
+        >
+          {title}
+        </h1>
         <ul className="flex flex-col gap-3 my-8">
           {points.map((el) => {
-            return <li className="text-xl font-bold font-mono" key={nanoid()}>{el}</li>;
+            return (
+              <div className="flex gap-2">
+                <div className="text-xl pt-1 text-sky-500"><IoMdArrowDropright /></div>
+                <li className="text-xl font-bold font-mono" key={nanoid()}>
+                  {el}
+                </li>
+              </div>
+            );
           })}
         </ul>
 
