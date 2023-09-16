cacf9524df5aa8bf5a99de596a8c144b82908a87
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-16 15:11:09 +0530

    Scroll Top Section

--- src/components/Scroll/ScrollToTop.jsx
+++ src/components/Scroll/ScrollToTop.jsx
@@ -6,15 +6,15 @@ function ScrollToTop() {
 
   useEffect(() => {
     const toggleVisibility = () => {
-        console.log(window.scrollY , isVisible)
         return window.scrollY > 1000 ? setIsVisible(true) : setIsVisible(false);
     };
 
     window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
   return isVisible ? (
-    <div data-aos='fade-right' className="text-3xl fixed right-5 top-3/4 cursor-pointer hover:bg-sky-500 p-2 rounded-full hover:transition-all hover:duration-300 hover:text-white hover:shadow-zinc-700 hover:shadow-md z-20 text-gray-600">
+    <div data-aos='fade-right' className="text-3xl fixed right-5 bottom-12 cursor-pointer hover:bg-sky-500 p-2 rounded-full hover:transition-all hover:duration-300 hover:text-white hover:shadow-zinc-700 hover:shadow-md z-20 text-gray-600">
       <a href="#top">
         <RiArrowUpSLine />
       </a>
