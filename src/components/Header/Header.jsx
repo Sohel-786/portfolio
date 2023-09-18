677fde25b402a759c99034aa91234c497361c3a2
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-19 00:10:34 +0530

    small fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,6 +20,7 @@ function Header() {
       className="flex justify-between items-center lg:px-20 lg:pr-14 py-4 px-6"
     >
       <div
+        data-aos-duration='700'
         data-aos="zoom-in-down"
         className=" flex justify-center items-center lg:text-lg text-base font-mono font-semibold"
         style={{
@@ -35,7 +36,7 @@ function Header() {
         />
         <Typewriter
           onInit={(typewriter) => {
-            typewriter.pauseFor(1500).typeString("Hello!").start();
+            typewriter.pauseFor(700).typeString("Hello!").start();
           }}
         />
       </div>
